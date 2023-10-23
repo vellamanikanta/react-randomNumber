b5072ea6f2564187494416d87dfdb1dba0712bc3
Manikanta <vellamanikanta8@gmail.com>
2023-10-23 02:58:52 +0000

    solution

---
+++ src/components/RandomNumberGenerator/index.js
@@ -0,0 +1,33 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class RandomNumberGenerator extends Component{
+    state = {count:0}
+
+    changeNumber = () =>{
+        const getRandom = Math.ceil(Math.random() * 100)
+
+        this.setState({count: getRandom})
+    }
+
+    render() {
+        const {count} = this.state 
+
+        return (
+            <div className ="container">
+                <div className = "card">
+                    <h1 className = "heading">Random Number</h1>
+                    <p>Generate a random number in the range of 0 to 100</p>
+                    <button type ="button" className ="button" onClick = {this.changeNumber}>
+                        Generate
+                    </button>
+                    <p className ="number">{count}</p>
+                </div>
+            </div>
+        )
+
+    }
+}
+export default RandomNumberGenerator
